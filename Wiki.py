2099d4d81a9d5f83849650042210cc0c408f8e48
vortex73 <32898329+vortex73@users.noreply.github.com>
2021-03-31 22:55:42 +0530

    Update Wiki.py

--- Wiki.py
+++ Wiki.py
@@ -4,11 +4,15 @@ def wiki(a):
     if a == 'wikipedia':
        main.Goverbal('Please ask your question ....')
        main.Input()
-       
+
     else:
         main.Goverbal('traversing through the wide collections of wikipedia...')
         a = a.replace("wikipedia", "")
-        results = wikipedia.summary(a, sentences=2)
+        result= wikipedia.search(a)
+        print(result)
+        b=int(input("Enter the number corresponding to your desired search result, starting from 0 -"))
+
+        results = wikipedia.summary(result[b] , sentences = 2, auto_suggest=False, redirect=True)
         main.Goverbal("According to Wiki")
         print(results)
         main.Goverbal(results)
